9a4b7562795e84ca2bc908cb16ed20be2ba53b20
enovak89 <enovak89@mail.ru>
2023-01-26 20:14:51 +0300

    ефыл1-3

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-        task1();
+        task1_2();
+        task3();
 
     }
 
-    public static void task1() {
+    public static void task1_2() {
+        System.out.println("task1_2");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("ФИО сотрудника - " + fullName);
+        System.out.println("ФИО сотрудника для отчета - " + fullName.toUpperCase());
 
+    }
+
+    public static void task3() {
+        System.out.println("task3");
+        String fullName = "Иванов Семён Семёнович";
+        System.out.println("ФИО сотрудника - " + fullName.replace('ё', 'е'));
     }
 
 }
